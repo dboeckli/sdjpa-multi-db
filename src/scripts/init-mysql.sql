b9f9b40508e33f76b4a22c07ead417cf848464f7
adm-boeckli <dboeckli@gmail.com>
2025-04-17 16:32:25 +0200

    fixup! initial commit 2

--- src/scripts/init-mysql.sql
+++ src/scripts/init-mysql.sql
@@ -1,35 +1,35 @@
 -- cardholder database
 DROP DATABASE IF EXISTS cardholderdb;
 DROP USER IF EXISTS `cardholderadmin`@`%`;
-DROP USER IF EXISTS `paymentuseradmin`@`%`;
+DROP USER IF EXISTS `cardholderuser`@`%`;
 CREATE DATABASE IF NOT EXISTS `cardholderdb` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER IF NOT EXISTS `cardholderadmin`@`%` IDENTIFIED BY 'password';
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW,
     CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `cardholderdb`.* TO `cardholderadmin`@`%`;
-CREATE USER IF NOT EXISTS `paymentuseradmin`@`%` IDENTIFIED BY 'password';
-GRANT SELECT, INSERT, UPDATE, DELETE, SHOW VIEW ON `cardholderdb`.* TO `paymentuseradmin`@`%`;
+CREATE USER IF NOT EXISTS `cardholderuser`@`%` IDENTIFIED BY 'password';
+GRANT SELECT, INSERT, UPDATE, DELETE, SHOW VIEW ON `cardholderdb`.* TO `cardholderuser`@`%`;
 FLUSH PRIVILEGES;
 
 -- card database
 DROP DATABASE IF EXISTS carddb;
 DROP USER IF EXISTS `cardadmin`@`%`;
-DROP USER IF EXISTS `carduseradmin`@`%`;
+DROP USER IF EXISTS `carduser`@`%`;
 CREATE DATABASE IF NOT EXISTS `carddb` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER IF NOT EXISTS `cardadmin`@`%` IDENTIFIED BY 'password';
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW,
 CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `carddb`.* TO `cardadmin`@`%`;
-CREATE USER IF NOT EXISTS `carduseradmin`@`%` IDENTIFIED BY 'password';
-GRANT SELECT, INSERT, UPDATE, DELETE, SHOW VIEW ON `carddb`.* TO `carduseradmin`@`%`;
+CREATE USER IF NOT EXISTS `carduser`@`%` IDENTIFIED BY 'password';
+GRANT SELECT, INSERT, UPDATE, DELETE, SHOW VIEW ON `carddb`.* TO `carduser`@`%`;
 FLUSH PRIVILEGES;
 
 -- card PAN database
 DROP DATABASE IF EXISTS pandb;
 DROP USER IF EXISTS `panadmin`@`%`;
-DROP USER IF EXISTS `panuseradmin`@`%`;
+DROP USER IF EXISTS `panuser`@`%`;
 CREATE DATABASE IF NOT EXISTS `pandb` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER IF NOT EXISTS `panadmin`@`%` IDENTIFIED BY 'password';
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW,
 CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `pandb`.* TO `panadmin`@`%`;
-CREATE USER IF NOT EXISTS `panuseradmin`@`%` IDENTIFIED BY 'password';
-GRANT SELECT, INSERT, UPDATE, DELETE, SHOW VIEW ON `pandb`.* TO `panuseradmin`@`%`;
+CREATE USER IF NOT EXISTS `panuser`@`%` IDENTIFIED BY 'password';
+GRANT SELECT, INSERT, UPDATE, DELETE, SHOW VIEW ON `pandb`.* TO `panuser`@`%`;
 FLUSH PRIVILEGES;
